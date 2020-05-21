d04d44140106d1725feb6ca84b6b244696751e6e
raghav-blacklist1 <raghav.blacklist1@gmail.com>
2020-05-21 21:10:34 +0530

    Get levels

--- src/btp/p2p/flashcrowd/init/InitNetwork.kt
+++ src/btp/p2p/flashcrowd/init/InitNetwork.kt
@@ -14,6 +14,7 @@ class InitNetwork(val prefix: String) : Control {
     private val dhtPid: Int = Configuration.getPid("$prefix.$PAR_DHT")
 
     override fun execute(): Boolean {
+
 //        (0 until Network.size()).forEach {
 //            val dhtProtocol = Network.get(it).getProtocol(dhtPid) as DHTProtocol
 //            EDSimulator.add(5 * 1000, StoreMessage<String>(dhtProtocol.address, "data $it"), Network.get(it), pid)
@@ -23,12 +24,12 @@ class InitNetwork(val prefix: String) : Control {
         val msg1 = FindValueOperation(pid, kademliaProtocol.nodeId, "xyz")
         val msg2 = StoreValueOperation(pid, kademliaProtocol.nodeId, "xyz", "value")
 
-        val node2 = Network.get(CommonState.r.nextInt(Network.size()))
-        val kademliaProtocol2 = node2.getProtocol(dhtPid) as KademliaProtocol
-        val msg3 = FindValueOperation(pid, kademliaProtocol2.nodeId, "xyz")
+//        val node2 = Network.get(CommonState.r.nextInt(Network.size()))
+//        val kademliaProtocol2 = node2.getProtocol(dhtPid) as KademliaProtocol
+//        val msg3 = FindValueOperation(pid, kademliaProtocol2.nodeId, "xyz")
         EDSimulator.add(1000, msg1, node, dhtPid)
         EDSimulator.add(3000, msg2, node, dhtPid)
-        EDSimulator.add(5000, msg3, node2, dhtPid)
+//        EDSimulator.add(15000, msg3, node2, dhtPid)
         return false
     }
 
--- src/btp/p2p/flashcrowd/init/InitNode.kt
+++ src/btp/p2p/flashcrowd/init/InitNode.kt
@@ -2,8 +2,26 @@ package btp.p2p.flashcrowd.init
 
 import peersim.core.Node
 import peersim.dynamics.NodeInitializer
+import peersim.config.Configuration
+import kotlin.math.floor
+import kotlin.math.log10
 
 class InitNode(prefix: String) : NodeInitializer {
+
+    private fun getlevel(rank: Int):Int{
+
+        val ans: Int = floor(log10((base-1)*rank.toDouble() + 1)/(log10(base.toDouble()) + 0.001)).toInt();
+        return ans+1
+    }
     override fun initialize(n: Node?) {
+
+        val rank = n?.getID();
+        val level = rank?.toInt()?.let { getlevel(it) }
+        println(rank.toString() + " " + level.toString())
+   }
+
+    companion object{
+        var rank = 1
+        var base = 4
     }
 }
